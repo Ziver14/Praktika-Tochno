b3ef4dae0be62eb359977ffe369a1b5d6f36395b
alexander <sanya_kab@mail.ru>
2024-02-21 19:59:42 +0300

    Добавили разбор 2

---
+++ Praktika Tochno/Main.cpp
@@ -0,0 +1,122 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<list>
+
+class Cat {
+private:
+
+	std::string name_;
+	std::string breed_;
+	std::string owner_;
+public:
+	Cat() : name_("noname"), breed_("nobreed"), owner_("noowner") {};
+
+	Cat(std::string name, std::string breed, std::string owner) {
+		name_ = name;
+		breed_ = breed;
+		owner_ = owner;
+	}
+
+	std::string  GetName() const {
+		return name_;
+	}
+	
+	bool operator == (const Cat& cat) {
+	return
+	name_ == cat.name_&& 
+	breed_ == cat.breed_&&
+	owner_ == cat.owner_;
+	}
+
+		
+		
+	~Cat() {};
+
+
+};
+
+
+class Hotel {
+private:
+	std::list <Cat> cats;
+
+	int maxCat = 5;
+	int NumOfCat_=0;
+public:
+	Hotel() {};
+
+	Hotel(int NumOfCat) {
+		NumOfCat_ = NumOfCat;
+	}
+
+
+
+	int GetNumOfCat () {
+		return NumOfCat_;
+	}
+
+	int SetNumOfCat(int NumOfCat){
+		NumOfCat_ = NumOfCat;
+	}
+
+
+	void addCat(const Cat& cat) {
+		NumOfCat_++;
+		if (NumOfCat_ > maxCat) throw - 1;
+		cats.push_back(cat);
+		std::cout << cat.GetName() <<' ' << " In" << std::endl;
+	}
+	void searchCat(const std::string& catName) {
+		for (const auto& cat : cats) {
+			if (cat.GetName() == catName) {
+				std::cout << "Here";
+				return;
+			}
+		}
+				std::cout << "Is not here";
+	};
+			
+			
+	
+	void deleteCat(const std::string& Cat) {
+		maxCat--;
+		
+		for (auto i = cats.begin(); i != cats.end(); i++) {
+			if (i->GetName() == Cat)
+			{
+				cats.erase(i);
+				i = cats.begin();
+			}
+			
+		}
+		
+	};
+		
+
+
+	~Hotel() {};
+
+};
+
+
+
+
+
+
+int main(){
+
+Hotel obj1;
+
+Cat cat2("Murka", "J", "J");
+
+Cat cat1("Barsic", "M", "M");
+
+obj1.addCat(cat1);
+std::cout << std::endl;
+obj1.addCat(cat2);
+std::cout << std::endl;
+obj1.searchCat("Murka");
+
+	return 0;
+}
---
+++ Praktika Tochno/Razbor.cpp
@@ -0,0 +1,124 @@
+﻿#include<iostream>
+#include<string>
+
+class Cat {
+public:
+	Cat(): Cat(" "," "," ") { }
+	Cat(std::string name, std::string bride, std::string owner):name_(name),bride_(bride),owner_(owner) { }
+
+	std::string Name()const {
+
+		return name_;
+	}
+	std::string Bride()const {
+		return bride_;
+	}
+	//возвращает значение поля
+	std::string Owner()const {
+		return owner_;
+	}
+
+	void Name(std::string name) {
+		name_ = name;
+	}
+	//устанавливает значение поля
+	void Bride(std::string bride) {
+		bride_ = bride;
+	}
+
+	void Owner(std::string owner) {
+		owner_ = owner;
+	}
+
+	bool operator==(const Cat& other) {
+		return name_ == other.name_ &&
+			bride_ == other.bride_ &&
+			owner_ == other.owner_;
+
+	}
+private:
+	std::string name_;
+	std::string bride_;
+	std::string owner_; 
+
+
+};
+
+
+class Hotel {
+public:
+	Hotel() :cats_(nullptr), current_(0), capacity_(0){ }
+
+	Hotel(int capacity) {
+		current_ = 0;
+		capacity_ = capacity;
+		cats_ = new Cat[capacity_]{};
+
+	}
+
+	//Добавление кота
+	void addCat(const Cat &cat) {
+		if (capacity_ > current_) {
+			cats_[current_] = cat;
+			current_++;
+		}
+		else {
+			throw - 1;
+		}
+	}
+	Cat deleteCat(int num) {
+		if (num > current_ || num < 1) {
+			throw - 1;
+		}
+		Cat result{ cats_[num - 1] };
+		std::swap(cats_[num - 1], cats_[current_ - 1]);
+		//тут может быть очиска данных последнего хранимого элемента
+		current_ -= 1;
+		return result;
+	}
+
+	void newCapacity(int i) {
+		if (i < current_) {
+			throw - 1;
+		}
+
+		Cat* tmp = new Cat[i]{};
+		for (size_t j = 0; j < current_; j++)
+		{
+			tmp[j] = cats_[j];
+		}
+		std::swap(tmp, cats_);
+		capacity_ = i;
+		delete[] tmp;
+	}
+
+	int findCat(std::string name_cat) {
+		int result{ -1 };
+		for (size_t i = 0; i < current_; i++)
+		{
+			if (cats_[i].Name() == name_cat) {
+				result = i + 1;
+				break;
+			}
+		}
+		return result;
+	}
+private:
+	Cat* cats_;		//коты	
+	int capacity_;  //вместимость отеля
+	int current_;	//текущее колличество
+};
+
+
+
+
+
+int main() {
+
+	Cat kitty;
+	kitty.Owner();
+
+
+
+	return 0;
+}
